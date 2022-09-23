76573aafa91fc797df3ae5b9cd156939aa41645b
Hyesu Lee <61389628+Sallyrideauto@users.noreply.github.com>
2022-09-23 15:51:54 +0900

    Time: 91 ms (37.68%), Space: 15.5 MB (17.24%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,16 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        # 데크 자료형 선언
-        strs: Deque = collections.deque()
-
-        for char in s:
-            if char.isalnum():
-                strs.append(char.lower())
-
-        # 팰린드롬 여부 판별
-        while len(strs) > 1:
-            if strs.popleft() != strs.pop():
-                return False
-
-        return True
+        s = s.lower()
+        
+        # 정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+        
+        return s == s[::-1]  # 슬라이싱
